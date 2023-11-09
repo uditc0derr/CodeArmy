42d919d50368c9be0a6560f83b092050a783004c
uditc0derr <uditcoderr@gmail.com>
2023-11-09 18:24:41 +0530

    sorting sentence

---
+++ String/Sorting_Sentence.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+string sortSentence(string s) {
+       vector<string>ans(10);
+       string temp;
+       int count=0,index=0;
+       while(index<s.size())
+       {
+           if(s[index]==' ')
+           {
+               int pos=temp[temp.size()-1]-'0';
+               temp.pop_back();
+               ans[pos]=temp;
+               temp.clear();
+               count++;
+           }
+           else
+           temp+=s[index];
+           index++;
+       } 
+       int pos=temp[temp.size()-1]-'0';
+               temp.pop_back();
+               ans[pos]=temp;
+               temp.clear();
+               count++;
+               for(int i=1;i<=count;i++)
+               {
+                   temp+=ans[i];
+                   temp+=' ';
+               }
+               temp.pop_back();
+               
+    return temp;
+    }
+int main(){
+    string s = "Udit3 is2 This1";
+    cout<<sortSentence(s);
+}
