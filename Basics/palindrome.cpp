038f555c1f1a6c6507aa704e6a9038be66fe259a
udit pandey <uditcoderr@gmail.com>
2024-01-28 18:05:34 +0530

    Add basics folder

---
+++ Basics/palindrome.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,temp,sum=0,r;
+    cout<<"Enter the value of n : ";
+    cin>>n;
+    temp = n;
+    while(n>0){
+        r =  n % 10;
+        sum = sum * 10 + r;
+        n = n / 10;
+    }
+    if(temp==sum){
+        cout<<"Palindrome Number ";
+    }else{
+        cout<<"Not palindrome Number ";
+    }
+}
