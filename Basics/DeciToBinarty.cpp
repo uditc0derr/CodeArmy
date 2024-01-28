54c73e4b1ba6e38fcf30e0a9566ce0fbe7b26386
udit pandey <uditcoderr@gmail.com>
2024-01-28 18:12:51 +0530

    DecimalToBinary

---
+++ Basics/DeciToBinarty.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,temp,r,sum=0,mult=1 ;
+    cout<<"Enter the n : ";
+    cin>>n;
+    temp=n;
+    while(n>0){
+        r = n%2;
+        sum = r*mult + sum ;
+        mult = mult * 10;
+        n = n / 2;
+
+    }
+    cout<<"Binary number of "<<temp<<" is : "<<sum;
+}
--- Basics/palindrome.cpp
+++ Basics/palindrome.cpp
@@ -1,18 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n,temp,sum=0,r;
-    cout<<"Enter the value of n : ";
-    cin>>n;
+int main()
+{
+    int n, temp, sum = 0, r;
+    cout << "Enter the value of n : ";
+    cin >> n;
     temp = n;
-    while(n>0){
-        r =  n % 10;
+    while (n > 0)
+    {
+        r = n % 10;
         sum = sum * 10 + r;
         n = n / 10;
     }
-    if(temp==sum){
-        cout<<"Palindrome Number ";
-    }else{
-        cout<<"Not palindrome Number ";
+    if (temp == sum)
+    {
+        cout << "Palindrome Number ";
+    }
+    else
+    {
+        cout << "Not palindrome Number ";
     }
 }
