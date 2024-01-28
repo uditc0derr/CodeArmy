0aa603e3a99d40f95cd4246557b79ade53621bba
udit pandey <uditcoderr@gmail.com>
2024-01-28 18:14:56 +0530

    Formatting

--- Basics/DeciToBinarty.cpp
+++ Basics/DeciToBinarty.cpp
@@ -1,16 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n,temp,r,sum=0,mult=1 ;
-    cout<<"Enter the n : ";
-    cin>>n;
-    temp=n;
-    while(n>0){
-        r = n%2;
-        sum = r*mult + sum ;
+int main()
+{
+    int n, temp, r, sum = 0, mult = 1;
+    cout << "Enter the n : ";
+    cin >> n;
+    temp = n;
+    while (n > 0)
+    {
+        r = n % 2;
+        sum = r * mult + sum;
         mult = mult * 10;
         n = n / 2;
-
     }
-    cout<<"Binary number of "<<temp<<" is : "<<sum;
+    cout << "Binary number of " << temp << " is : " << sum;
 }
